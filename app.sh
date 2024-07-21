d1998a266fc9645ce367b865084549e3df12d0a7
kairat <112894056+kairat-genius@users.noreply.github.com>
2024-07-21 13:02:32 +0300

    Update app.sh

--- app.sh
+++ app.sh
@@ -4,6 +4,4 @@ python manage.py makemigrations
 
 python manage.py migrate
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(username='admin', email='', password='admin')" | python manage.py shell
-
 gunicorn --config gunicorn_config.py settings.wsgi:application
